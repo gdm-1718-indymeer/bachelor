c83af5ff189c37dcbd7220e3b68471efc56c3747
indymeermans <indymeermans@hotmail.com>
2021-06-30 02:00:33 +0200

    Custom calendar

--- bachelor/src/pages/Home.js
+++ bachelor/src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Calendar from "../components/Calendar";
 
 const Home = (props) => {
     return (
         <div>
             <h1>reeee</h1>
+            <div className='container'>
+             <Calendar />
+            </div>
         </div>
     )
 }
