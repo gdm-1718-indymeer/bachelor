8057b4d069e469b6be3299ed054df81d3f97ba7d
LAPTOP-PHRGLPTN\awany <indymeermans@hotmail.com>
2021-08-07 22:27:08 +0200

    Fix normal login

--- bachelor/src/components/layout/BaseLayout.js
+++ bachelor/src/components/layout/BaseLayout.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import { Redirect } from 'react-router-dom'
-import { Navigation } from './index'
-import firebase from '../../config/firebaseConfig'
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { Navigation } from './index';
+import firebase from '../../config/firebaseConfig';
 
 function BaseLayout(props) {
-  const { window, children } = props
+  const { window, children } = props;
 
-  if (!firebase.auth().currentUser) {
+  /*if (!firebase.auth().currentUser) {
     return < Redirect to='/login' />
   }
-
+*/
   return (
     <div>
       {children}
       <Navigation path={props.location.pathname}></Navigation>
-
     </div>
-  )
+  );
 }
 
-export default BaseLayout
+export default BaseLayout;
