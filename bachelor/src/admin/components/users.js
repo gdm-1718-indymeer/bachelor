cdac2069aab8bb997d9374af9bc6e64b7922ebde
LAPTOP-PHRGLPTN\awany <indymeermans@hotmail.com>
2021-08-08 12:02:53 +0200

    cleanup errors from dashboard

--- bachelor/src/admin/components/users.js
+++ bachelor/src/admin/components/users.js
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const Users = () => {
-    return (
-        <div class="container">
-            <div class="user-cards">
-                <div class="user-card">
-                    <i class="fas fa-ellipsis-v"></i>
-                    <div class="image-wrapper">
-                         <img src="https://source.unsplash.com/H982yXJ7vOk/120x120" />
-                    </div>
-                    <p class="user-name">Henry Paulista</p>
-                    <p class="user-mail">henry.p@gmail.com</p>
-                   
-                    <p class="user-role">SENIOR CREATIVE DIRECTOR</p>
-                </div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className='container'>
+      <div className='user-cards'>
+        <div className='user-card'>
+          <i className='fas fa-ellipsis-v'></i>
+          <div className='image-wrapper'>
+            <img src='https://source.unsplash.com/H982yXJ7vOk/120x120' />
+          </div>
+          <p className='user-name'>Henry Paulista</p>
+          <p className='user-mail'>henry.p@gmail.com</p>
 
-export default Users
+          <p className='user-role'>SENIOR CREATIVE DIRECTOR</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Users;
--- bachelor/src/components/layout/DashboardLayout.js
+++ bachelor/src/components/layout/DashboardLayout.js
@@ -1,132 +1,147 @@
-import React, {useState, useCallback} from 'react'
+import React, { useState, useCallback } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendar, faChartBar, faCog,  faHome,  faPowerOff, faUser } from '@fortawesome/free-solid-svg-icons';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import {
+  faCalendar,
+  faChartBar,
+  faCog,
+  faHome,
+  faPowerOff,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 
 const menuItems = {
-    'Dashboard': {
-        title: 'Dashboard',
-        icon: faHome
-    },
-    'Clients': {
-        title: 'Clients',
-        icon: faUser
-    },
-    'Statistics': {
-        title: 'Statistics',
-        icon: faChartBar
-    },
-    'Schedule': {
-        title: 'schedules',
-        icon: faCalendar
-    },
-    'Settings': {
-        title: 'Settings',
-        icon: faCog
-    }
-}
+  Dashboard: {
+    title: 'Dashboard',
+    icon: faHome,
+  },
+  Clients: {
+    title: 'Clients',
+    icon: faUser,
+  },
+  Statistics: {
+    title: 'Statistics',
+    icon: faChartBar,
+  },
+  Schedule: {
+    title: 'schedules',
+    icon: faCalendar,
+  },
+  Settings: {
+    title: 'Settings',
+    icon: faCog,
+  },
+};
 
 const BaseLayout = (props) => {
-    const { window, children } = props
+  const { window, children } = props;
 
-    const [sideBarReduced, setSideBarReduced] = useState(true)
-    const [activeMenuItem, setActiveMenuItem] = useState(null)
+  const [sideBarReduced, setSideBarReduced] = useState(true);
+  const [activeMenuItem, setActiveMenuItem] = useState(null);
 
+  let menuChangeActive = (index) => () => {
+    setActiveMenuItem(index);
+  };
 
-	let menuChangeActive = (index) => () => {
-        setActiveMenuItem(index)
-	};
+  const renderMenuItems = useCallback(() => {
+    const menuItemsComponentArray = [];
+    Object.values(menuItems).forEach((el, index) => {
+      menuItemsComponentArray.push(
+        !sideBarReduced ? (
+          <li
+            key={index}
+            onClick={menuChangeActive(index)}
+            className={`c-menu__item ${
+              index === activeMenuItem && 'is-active'
+            }`}>
+            <div className='c-menu__item__inner'>
+              <FontAwesomeIcon className='icon' icon={el.icon} />
+              <div className='c-menu-item__title'>
+                <span>{el.title}</span>
+              </div>
+            </div>
+          </li>
+        ) : (
+          <OverlayTrigger
+            key={index}
+            placement='right'
+            overlay={<Tooltip style={{ marginLeft: 5 }}>{el.title}</Tooltip>}>
+            <li
+              onClick={menuChangeActive(index)}
+              className={`c-menu__item ${
+                index === activeMenuItem && 'is-active'
+              }`}>
+              <div className='c-menu__item__inner'>
+                <FontAwesomeIcon className='icon' icon={el.icon} />
 
-    const renderMenuItems = useCallback(() => {
-        const menuItemsComponentArray = []
-        Object.values(menuItems).forEach((el, index) => {
-            menuItemsComponentArray.push(
-                !sideBarReduced ? (
-                <li onClick={menuChangeActive(index)} class={`c-menu__item ${index === activeMenuItem && 'is-active'}`} >
-                    <div class="c-menu__item__inner">
-                        <FontAwesomeIcon className="icon" icon={el.icon}/>
-                        <div class="c-menu-item__title">
-                            <span>{el.title}</span>
-                        </div>
-                    </div>
-                </li>
-                ) : (
-                    <OverlayTrigger placement="right" overlay={
-                        <Tooltip style={{marginLeft: 5}}>{el.title}</Tooltip>
-                    }>
-                        <li onClick={menuChangeActive(index)} class={`c-menu__item ${index === activeMenuItem && 'is-active'}`}>
-                            <div class="c-menu__item__inner">
-                                <FontAwesomeIcon className="icon" icon={el.icon}/>
+                <div className='c-menu-item__title'>
+                  <span>{el.title}</span>
+                </div>
+              </div>
+            </li>
+          </OverlayTrigger>
+        )
+      );
+    });
+    return menuItemsComponentArray;
+  }, [activeMenuItem, sideBarReduced]);
 
-                                <div class="c-menu-item__title">
-                                    <span>{el.title}</span>
-                                </div>
-                            </div>
-                        </li>
-                    </OverlayTrigger>
-
-            ))
-        })
-        return menuItemsComponentArray
-    },[activeMenuItem, sideBarReduced])
-
-	const sidebarChangeWidth = () => {
-        setSideBarReduced(prevState => !prevState)
-	
-	};
+  const sidebarChangeWidth = () => {
+    setSideBarReduced((prevState) => !prevState);
+  };
 
   return (
-    <div>                
-        <body class={sideBarReduced ? "sidebar-is-reduced" : "sidebar-is-expanded"}>
-            <header class="l-header">
-                <div class="l-header__inner clearfix">
-                    <div class="c-header-icon js-hamburger"  onClick={sidebarChangeWidth}>
-                        <div class={`hamburger-toggle ${!sideBarReduced && "is-opened"}`}>
-                            <span class="bar-top"></span>
-                            <span class="bar-mid"/>
-                            <span class="bar-bot"/>
-                        </div>
-                    </div>
-                    <div class="c-header-icon has-dropdown">
-                        <span class="c-badge c-badge--red c-badge--header-icon animated swing">13</span>
-                        <i class="fa fa-bell"></i>
-                        <div class="c-dropdown c-dropdown--notifications">
-                            <div class="c-dropdown__header"></div>
-                            <div class="c-dropdown__content"></div>
-                        </div>
-                    </div>
-       
-                    <div class="header-icons-group">
-                        <div class="c-header-icon logout">        
-                            <FontAwesomeIcon className="icon" icon={faPowerOff}/>
-                        </div>
-                    </div>
-                </div>
-            </header>
-        <div class="l-sidebar">
-            <div class="logo">
-            <div class="logo__txt">D</div>
+    <div
+      className={sideBarReduced ? 'sidebar-is-reduced' : 'sidebar-is-expanded'}>
+      <header className='l-header'>
+        <div className='l-header__inner clearfix'>
+          <div
+            className='c-header-icon js-hamburger'
+            onClick={sidebarChangeWidth}>
+            <div
+              className={`hamburger-toggle ${!sideBarReduced && 'is-opened'}`}>
+              <span className='bar-top'></span>
+              <span className='bar-mid' />
+              <span className='bar-bot' />
             </div>
-            <div class="l-sidebar__content">
-            <nav class="c-menu js-menu">
-                <ul class="u-list">
-                {renderMenuItems()}
-                </ul>
-            </nav>
+          </div>
+          <div className='c-header-icon has-dropdown'>
+            <span className='c-badge c-badge--red c-badge--header-icon animated swing'>
+              13
+            </span>
+            <i className='fa fa-bell'></i>
+            <div className='c-dropdown c-dropdown--notifications'>
+              <div className='c-dropdown__header'></div>
+              <div className='c-dropdown__content'></div>
             </div>
+          </div>
+
+          <div className='header-icons-group'>
+            <div className='c-header-icon logout'>
+              <FontAwesomeIcon className='icon' icon={faPowerOff} />
+            </div>
+          </div>
         </div>
+      </header>
+      <div className='l-sidebar'>
+        <div className='logo'>
+          <div className='logo__txt'>D</div>
+        </div>
+        <div className='l-sidebar__content'>
+          <nav className='c-menu js-menu'>
+            <ul className='u-list'>{renderMenuItems()}</ul>
+          </nav>
+        </div>
+      </div>
 
-        <main class="l-main">
-            <div class="content-wrapper content-wrapper--with-bg">
-                {children}
-
-            </div>
-        </main>
-        </body>
-
+      <main className='l-main'>
+        <div className='content-wrapper content-wrapper--with-bg'>
+          {children}
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default BaseLayout
+export default BaseLayout;
