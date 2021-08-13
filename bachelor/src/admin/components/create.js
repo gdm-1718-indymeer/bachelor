29f5c2c1295fc45bffb4d42f29882bde532b1e46
indy <indymeermans@hotmail.com>
2021-08-13 19:50:15 +0200

    fixed react quill issues

--- bachelor/src/admin/components/create.js
+++ bachelor/src/admin/components/create.js
@@ -1,28 +1,28 @@
-import React, { useState, useEffect , useCallback} from 'react'
-import { Container , Form, Row, Col, Button} from 'react-bootstrap'
+import React, { useState, useEffect, useCallback } from 'react'
+import { Container, Form, Row, Col, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
-import {addMedication} from '../../services/auth.services';
+import { addMedication } from '../../services/auth.services';
 import Lottie from 'react-lottie';
 import sitting from '../../assets/lotties/sitting.json';
 import ReactQuill from 'react-quill'; // ES6
 import 'react-quill/dist/quill.snow.css'; // ES6
 
 const Create = (props) => {
-    const [state, setState] = useState({});
-    const [formdata, setFormdata] = useState({});
-    const [message, setMessage] = useState(false);
+  const [state, setState] = useState({});
+  const [formdata, setFormdata] = useState({});
+  const [message, setMessage] = useState(false);
 
-    useEffect (() => {
-      if(props.location.state){
-        setFormdata(props.location.state)
-      }
+  useEffect(() => {
+    if (props.location.state) {
+      setFormdata(props.location.state)
+    }
   }, []);
 
-    const onChange = (e) => {
-      const name = e.target.name;
-      const newState = { ...formdata };
-      newState[name] = e.target.value;
-      setFormdata(newState);
+  const onChange = (e) => {
+    const name = e.target.name;
+    const newState = { ...formdata };
+    newState[name] = e.target.value;
+    setFormdata(newState);
   };
 
   const defaultOptions = {
@@ -34,70 +34,69 @@ const Create = (props) => {
     }
   };
 
-    const onSubmit = async (e) => {
-      let currentUser = JSON.parse(localStorage.getItem('firebase:currentUser'));
-      const uid = currentUser.uid;
-      if(formdata.name, formdata.type, formdata.inname, formdata.description, formdata.warn){
-        if (formdata.name ) {
+  const onSubmit = async (e) => {
+    let currentUser = JSON.parse(localStorage.getItem('firebase:currentUser'));
+    const uid = currentUser.uid;
+    if (formdata.name, formdata.type, formdata.inname, formdata.description, formdata.warn) {
+      if (formdata.name) {
 
-          let data = {
-            name: formdata.name,
-            value: formdata.value,
-            label: formdata.name,
-            type: formdata.type,
-            inname: formdata.inname,
-            description: formdata.description,
-            warn: formdata.warn
-          }
-          
-          const result = await addMedication(data);
-
-          if (!result.message) {
-            setMessage({
-              succeed: 'De data is succesvol toegevoegd.',
-            });
-            setFormdata({name: "", value: "", type: "", inname: "", description: "", warn: ""})
-            setTimeout(() => {
-              setMessage({});
-            }, 3000);
-          } else if (result.message) {
-            setMessage({
-              error: result.message,
-            });
-          }
-        } else {
-          setMessage({
-            error: 'Medicijnnaam niet ingevuld',
-          });
-          return;
+        let data = {
+          name: formdata.name,
+          value: formdata.value,
+          label: formdata.name,
+          type: formdata.type,
+          inname: formdata.inname,
+          description: formdata.description,
+          warn: formdata.warn
         }
-      }else {
+
+        const result = await addMedication(data);
+
+        if (!result.message) {
+          setMessage({
+            succeed: 'De data is succesvol toegevoegd.',
+          });
+          setFormdata({ name: "", value: "", type: "", inname: "", description: "", warn: "" })
+          setTimeout(() => {
+            setMessage({});
+          }, 3000);
+        } else if (result.message) {
+          setMessage({
+            error: result.message,
+          });
+        }
+      } else {
         setMessage({
-          error: 'Niet alle velden zijn ingevuld',
+          error: 'Medicijnnaam niet ingevuld',
         });
-        setTimeout(() => {
-          setMessage({});
-        }, 3000);
-        return
+        return;
       }
+    } else {
+      setMessage({
+        error: 'Niet alle velden zijn ingevuld',
+      });
+      setTimeout(() => {
+        setMessage({});
+      }, 3000);
+      return
+    }
 
-    };
-
-    return (
-      <Container style={{ marginTop: '100px' }}>
+  };
+  return (
+    <Container style={{ marginTop: '100px' }}>
       <div className='dashboard-intro'>
         <div className='row'>
           <div className='col-4'>
-            <Lottie 
-                options={defaultOptions}
-                height={'100%'}
-                width={'100%'}
-                className={'test'}
+            <Lottie
+              options={defaultOptions}
+              height={'100%'}
+              width={'100%'}
+              className={'test'}
             />
           </div>
           <div className='col-8 dashboard-intro__text'>
-            <p>Het kan zijn dat sommige medicijnen een <b>3e partij </b>zijn van de <b>originele medicijn</b>. 
-            Voeg de originele medicijn als 2e waarde om de juist mogelijke weergaven te bemachtigen. </p>
+            <p>Het kan zijn dat sommige medicijnen een <b>3e partij </b>zijn van de <b>originele medicijn</b>.
+              Voeg de originele medicijn als 2e waarde om de juist mogelijke weergaven te bemachtigen. </p>
             <p>Indien het de originele medicijn is laat je de 2e waarde leeg. </p>
           </div>
         </div>
@@ -106,25 +105,25 @@ const Create = (props) => {
         <Form.Row >
           <h3 className='pb-50'>Voeg hier je medicijn toe</h3>
           {message.error && (
-              <p className='alert alert-danger'>{message.error}</p>
-            )}
-            {message.succeed && (
-              <p className='alert alert-success'>{message.succeed}</p>
-            )}
+            <p className='alert alert-danger'>{message.error}</p>
+          )}
+          {message.succeed && (
+            <p className='alert alert-success'>{message.succeed}</p>
+          )}
           <Col className='medicine-form__input'>
             <Form.Label>Medicijnnaam</Form.Label>
-            <Form.Control placeholder="Medicijn naam" name='name' id='name' value={formdata.name} onChange={onChange}/>
+            <Form.Control placeholder="Medicijn naam" name='name' id='name' value={formdata.name} onChange={onChange} />
           </Col>
           <Col className='medicine-form__input'>
             <Form.Label>Hoofdmedicijn (laat leeg indien dit de hoofdmedicijn is)</Form.Label>
-            <Form.Control placeholder="Medicijn 2e waarde" name='value' id='value' value={formdata.value} onChange={onChange}/>
+            <Form.Control placeholder="Medicijn 2e waarde" name='value' id='value' value={formdata.value} onChange={onChange} />
           </Col>
 
           <Col className='medicine-form__input'>
             <Form.Label>Medicijn type</Form.Label>
-            <Form.Control as="select"  value={formdata.type}
-            onChange={e => { setFormdata({...formdata, type: e.target.value })}}>
-              <option   selected>Medicijn type</option>
+            <Form.Control as="select" value={formdata.type}
+              onChange={e => { setFormdata({ ...formdata, type: e.target.value }) }}>
+              <option selected>Medicijn type</option>
               <option value="zalf">Zalf</option>
               <option value="tablet">Tablet</option>
               <option value="capsule">Capsule</option>
@@ -137,9 +136,9 @@ const Create = (props) => {
 
           <Col className='medicine-form__input'>
             <Form.Label>Medicijn Inname</Form.Label>
-            <Form.Control as="select"  value={formdata.inname}
-              onChange={e => { setFormdata({...formdata, inname: e.target.value })}}>
-              <option   selected>Inname</option>
+            <Form.Control as="select" value={formdata.inname}
+              onChange={e => { setFormdata({ ...formdata, inname: e.target.value }) }}>
+              <option selected>Inname</option>
               <option value="oor">In het oor</option>
               <option value="oog">In het oog</option>
               <option value="oraal">Oraal</option>
@@ -152,22 +151,22 @@ const Create = (props) => {
 
           <Col className='medicine-form__input'>
             <Form.Label>Uitleg</Form.Label>
-              <ReactQuill key={Math.random()} value={formdata.description} name='description' onChange={(html) => setFormdata({...formdata, description: html})} />
+            <ReactQuill value={formdata.description || props.location.state.description || ''} name='description' onChange={(html) => setFormdata({ ...formdata, description: html })} />
           </Col>
 
           <Col className='medicine-form__input'>
             <Form.Label>Bijsluiters (bv. max 4uur tussen)</Form.Label>
-              <ReactQuill modules={this.modules} key={Math.random()} value={formdata.warn} name='warn' onChange={(html) => setFormdata({...formdata, warn: html})} />
+            <ReactQuill value={formdata.warn || props.location.state.warn || ''} name='warn' onChange={(html) => setFormdata({ ...formdata, warn: html })} />
           </Col>
-          
+
         </Form.Row>
         <Link className='btn white' to='/dashboard/medication' onClick={onSubmit}>Keer terug</Link>
         <Button style={{ margin: '30px 0' }} onClick={onSubmit}>Medicijn toevoegen</Button>
 
       </Form>
-      </Container>
-   
-    )
+    </Container>
+
+  )
 }
 
 export default Create
--- bachelor/src/admin/components/list.js
+++ bachelor/src/admin/components/list.js
@@ -1,49 +1,51 @@
-import React,  { useState, useEffect, useCallback } from 'react'
-import { Button} from 'react-bootstrap';
+import React, { useState, useEffect, useCallback } from 'react'
+import { Button } from 'react-bootstrap';
 import { deleteMedication } from '../../services/auth.services';
 import Popup from '../components/popup';
 import { Link } from 'react-router-dom';
 
 const List = (props) => {
 
-    const [isOpen, setIsOpen] = useState(false);
- 
-    const togglePopup = () => {
-      setIsOpen(!isOpen);
-    }
+  const [isOpen, setIsOpen] = useState(false);
 
-    const deleteItems = (id) => async () => {
-        await deleteMedication(id)
-        togglePopup()
-        props.update()
-    }
-  
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
 
-    useEffect(() => {
-    }, [props.rows]);
+  const deleteItems = (id) => async () => {
+    await deleteMedication(id)
+    togglePopup()
+    props.update()
+  }
 
-    return Object.keys(props.rows).map((obj, i) => (
-        <tr>
-            <td>{i+1}</td>
-            <td>{props.rows[obj].name}</td>
-            <td>{props.rows[obj].value}</td>
-            <td><Link className='btn white' to={{ pathname: '/dashboard/medication/create', state: props.rows[obj] }}>Update</Link> <Button variant="danger" onClick={togglePopup}>Verwijderen</Button>
-            </td>
-            {isOpen && 
-            <Popup
-            content={<div className='popup-content'>
-                  <h1>Verwijder de medicatie</h1>
-                  <p>Ben je zeker dat je deze medicatie wilt verwijderen?</p>
-                  
-                  <div class="clearfix">
-                      <button type="button"  onClick={togglePopup} class="cancelbtn">Cancel</button>
-                      <button type="button"  class="deletebtn" onClick={deleteItems(props.rows[obj].name)}>Verwijderen</button>
-                  </div>
-      </div>}
-      handleClose={togglePopup}
-    />}
-      </tr>
-    ))
+
+  useEffect(() => {
+  }, [props.rows]);
+
+  return Object.keys(props.rows).map((obj, i) =>
+  (
+    <tr>
+      <td>{i + 1}</td>
+      <td>{props.rows[obj].name}</td>
+      <td>{props.rows[obj].value}</td>
+      <td><Link className='btn white' to={{ pathname: '/dashboard/medication/create', state: props.rows[obj] }}>Update</Link> <Button variant="danger" onClick={togglePopup}>Verwijderen</Button>
+      </td>
+      {isOpen &&
+        <Popup
+          content={<div className='popup-content'>
+            <h1>Verwijder de medicatie</h1>
+            <p>Ben je zeker dat je deze medicatie wilt verwijderen?</p>
+
+            <div class="clearfix">
+              <button type="button" onClick={togglePopup} class="cancelbtn">Cancel</button>
+              <button type="button" class="deletebtn" onClick={deleteItems(props.rows[obj].name)}>Verwijderen</button>
+            </div>
+          </div>}
+          handleClose={togglePopup}
+        />}
+    </tr>
+  )
+  )
 }
 
 export default List;
